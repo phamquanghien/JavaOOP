30cbb777f7875bb9f3948dff18fe00b5bb52549b
phamquanghien <pqh.cntt@gmail.com>
2023-02-21 09:32:47 +0700

    Sum the digits of a number

--- Lap2/src/App.java
+++ Lap2/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int myNumber, sumDigit = 0;
+        System.out.println("Enter an integer: ");
+        myNumber = sc.nextInt();
+        while(myNumber > 0)
+        {
+            sumDigit += myNumber % 10;
+            myNumber = myNumber / 10;
+        }
+        System.out.println("The sum of the digits of the above number is: " + sumDigit);
     }
 }
